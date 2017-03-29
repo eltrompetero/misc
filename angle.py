3e0173a1a7580c065574502db79675394344db6d
Eddie Lee <edl56@cornell.edu>
2017-03-29 18:23:45 -0400

    added angle.py

---
+++ angle.py
@@ -0,0 +1,20 @@
+# Module for manipulating angle/phase data.
+# Author: Edward D. Lee
+# Email: edl56@cornell.edu
+# 2017-03-29
+import numpy as np
+
+def phase_dist(phi1,phi2):
+    """
+    Phase error in each moment of time this is a maximum of pi at each moment in time.
+    
+    Params:
+    -------
+    phi1,phi2 (ndarrays)
+        n_time,n_dim
+    """
+    shape = phi1.shape
+    
+    dist = np.abs(phi1-phi2).ravel()
+    dist[dist>np.pi] = np.pi - dist[dist>np.pi]%np.pi
+    return dist.reshape(shape)
