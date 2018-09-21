fc00eea43b82b698b75f18cd4c9cd3d6fe78297b
eltrompetero <edl56@cornell.edu>
2018-09-21 14:35:45 -0400

    added radau quadrature methods

--- calculus.py
+++ calculus.py
@@ -1,7 +1,62 @@
 import numpy as np
+from scipy.special import binom
 
 
-class QuadGauss(object):
+def legcoeffs(n,k):
+    """
+    Generate coefficients for all polynomial terms of the Legendre polynomials using recursive
+    relations.  
+
+    Parameters
+    ----------
+    n : int
+    k : int
+    
+    Returns
+    -------
+    coeffs : ndarray
+        Vector of coefficients descending from x^n to x^0.
+    """
+    
+    from scipy.special import binom
+    return 2**n * binom(n,k) * binom((n+k-1)/2,n)
+
+def legjacobi(n):
+    """Jacobi matrix for nth order Gauss-Legendre quadrature.
+    """
+
+    J=np.zeros(n**2)
+    normalization=( legcoeffs( np.arange(n-1), np.arange(n-1) ) /
+                    legcoeffs( np.arange(1, n), np.arange(1, n) ) )
+    J[list(range(1,n**2,n+1))]=np.sqrt((2*np.arange(1,n)-1) / (2*np.arange(1,n)+1)) * normalization
+    J[list(range(n,n**2,n+1))]=np.sqrt((2*np.arange(1,n)-1) / (2*np.arange(1,n)+1)) * normalization
+    return J.reshape(n,n)
+
+def leggauss(n,x0=None):
+    """
+    Abscissa and weights for Gauss-Legendre quadrature.
+
+    Parameters
+    ----------
+    n : int
+
+    Returns
+    -------
+    x : ndarray
+    weights : ndarray
+    """
+    
+    J=legjacobi(n)
+    if x0 is None:
+        x,vec=np.linalg.eigh(J)
+        return x,vec[0]**2 * 2
+    else:
+        from numpy.polynomial.legendre import legval
+        J[-1,-1]=x0 - J[-1,-2]**2 * legval(x0, [0]*(n-2)+[1]) / legval(x0, [0]*(n-1)+[1])
+        x,vec=np.linalg.eigh(J)
+        return x,vec[0]**2 * 2
+
+class QuadGauss():
     def __init__(self,order,method='legendre',lobatto=False):
         """
         Straightforward gaussian integration using Chebyshev polynomials with mapping of the
@@ -92,6 +147,41 @@ class QuadGauss(object):
         return ( ( ( f(xgrid,ygrid)/self.W[:,None] )*self.weights[:,None] * (y1-y0)/2 ).sum(0) / 
                  self.W * self.weights * (x1-x0)/2 ).sum()
 #end QuadGauss
+
+
+class QuadGaussRadau(QuadGauss):
+    def __init__(self,order,method='legendre'):
+        """
+        Straightforward gaussian integration using Chebyshev polynomials with mapping of the
+        bounds into [-1,1]. Most useful for a bounded interval.
+
+        Parameters
+        ----------
+        order : int
+            Order of basis expansion.
+        method : str,'legendre'
+        lobatto : bool,False
+            If True, use Lobatto collocation points. Only works for Chebyshev polynomials.
+        """
+        from numpy.polynomial.chebyshev import chebval,chebgauss
+        
+        self.order = order
+        self.N = order
+        
+        if method=='legendre':
+            from numpy.polynomial.legendre import legval
+            self.basis = [lambda x,i=i:legval(x,[0]*i+[1]) for i in range(self.N+1)]
+            self.coX,self.weights=leggauss(self.N+1,x0=-1.)
+            self.basisCox = [b(self.coX) for b in self.basis]
+            self.W=np.ones_like(self.coX)
+
+        else: raise Exception("Invalid basis choice.")
+        
+        # Map bounds to given bounds or from given bounds to [-1,1].
+        self.map_to_bounds = lambda x,x0,x1: (x+1)/2*(x1-x0) + x0
+        self.map_from_bounds = lambda x,x0,x1: (x-x0)/(x1-x0)*2. - 1.
+#end QuadGaussRadau
+
 
 
 def finite_diff( mat,order,dx=1,**kwargs ):
--- test_calculus.py
+++ test_calculus.py
@@ -1,5 +1,41 @@
-from calculus import *
+from .calculus import *
 
+
+def test_leggauss():
+    """Check up to to deg=10."""
+
+    import numpy.polynomial.legendre as npleg
+    from scipy.integrate import quad
+    
+    # Check that weights and abscissa line up with numpy module.
+    for d in range(2,10):
+        myx,myw=leggauss(d)
+        x,w=npleg.leggauss(d)
+        assert np.isclose(myx,x).all() and np.isclose(myw,w).all()
+
+    # Check that quadrature with these work for both regular and Radau quadrature.
+    mxPolyOrder=6
+
+    # Legendre quadrature
+    x,w=leggauss(mxPolyOrder+1)
+    for po in range(1,mxPolyOrder+1):
+        f=lambda x:x**po
+
+        if (po%2)==1:
+            assert np.isclose(f(x).dot(w),0)
+        else:
+            trueVal=2./(po+1)
+            print(f(x).dot(w),trueVal)
+            assert np.isclose(f(x).dot(w),trueVal)
+
+    x,w=leggauss(mxPolyOrder+1,x0=-1.)
+    for po in range(1,mxPolyOrder+1):
+        f=lambda x:x**po
+        
+        if (po%2)==0:
+            trueVal=2./(po+1)
+            print(f(x).dot(w),trueVal)
+            assert np.isclose(f(x).dot(w),trueVal), (po, f(x).dot(w), trueVal)
 
 def test_quad():
     from scipy.integrate import quad
@@ -21,3 +57,15 @@ def test_quad():
     for po in range(1,mxPolyOrder+1):
         f=lambda x:x**po/np.sqrt(1-x**2)
         assert np.isclose(qg.quad(f,-1,1),quad(f,-1,1)[0])
+    
+    # Radau-Gauss
+    # Legendre quadrature
+    qg=QuadGaussRadau(10)
+    for po in range(1,mxPolyOrder+1):
+        f=lambda x:x**po
+
+        if (po%2)==1:
+            assert np.isclose(qg.quad(f,-1,1),0)
+        else:
+            trueVal=2./(po+1)
+            assert np.isclose(qg.quad(f,-1,1),trueVal)
